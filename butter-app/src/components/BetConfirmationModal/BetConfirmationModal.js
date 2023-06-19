0faccc2945b5a7b9e2c29231ad001be349fbe298
Avi Desai <avidesai2@gmail.com>
2023-06-19 02:06:21 -0700

    added a bet confirmation modal

--- butter-app/src/components/BetConfirmationModal/BetConfirmationModal.js
+++ butter-app/src/components/BetConfirmationModal/BetConfirmationModal.js
@@ -1,17 +1,43 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import Confetti from 'react-confetti';
 import './BetConfirmationModal.css';
 
 const BetConfirmationModal = ({ title, choice, betAmount, closeModal }) => {
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      closeModal();
+    }, 5000); // Close the confirmation modal after 5 seconds
+
+    return () => clearTimeout(timer);
+  }, [closeModal]);
+
   return (
     <div className="confirmation-overlay">
       <div className="confirmation-content">
-        <h2>Bet Confirmation</h2>
-        <p>Title: {title}</p>
-        <p>Choice: {choice}</p>
-        <p>Bet Amount: {betAmount}</p>
+        <button onClick={closeModal} className="bet-confirmation-close-button">X</button>
+        <h2 className="confirmation-title">Bet Confirmation</h2>
+        <p className="confirmation-message">Your bet has been placed successfully!</p>
+        <div className="confirmation-details">
+          <p className="confirmation-label">Bet:</p>
+          <p className="confirmation-value">{title}</p>
+        </div>
+        <div className="confirmation-details">
+          <p className="confirmation-label">Choice:</p>
+          <p className="confirmation-value">{choice}</p>
+        </div>
+        {/* <div className="confirmation-details">
+          <p className="confirmation-label">Bet Amount:</p>
+          <p className="confirmation-value">{betAmount}</p>
+        </div> */}
       </div>
-      <Confetti width={window.innerWidth} height={window.innerHeight} recycle={false} />
+      <Confetti
+        width={window.innerWidth}
+        height={window.innerHeight}
+        recycle={false}
+        numberOfPieces={800}
+        gravity={0.15}
+        colors={['#f44336', '#e91e63', '#9c27b0', '#673ab7', '#3f51b5', '#2196f3', '#03a9f4', '#00bcd4', '#009688', '#4caf50', '#8bc34a', '#cddc39', '#ffeb3b', '#ffc107', '#ff9800', '#ff5722', '#795548']}
+      />
     </div>
   );
 };
--- butter-app/src/components/BetModal/BetModal.js
+++ butter-app/src/components/BetModal/BetModal.js
@@ -1,10 +1,12 @@
 import React, { useState } from 'react';
 import './BetModal.css';
+import BetConfirmationModal from '../BetConfirmationModal/BetConfirmationModal';
 
 const BetModal = ({ isOpen, closeModal, handleBetAmount, data }) => {
   const { title, price, choices } = data;
   const [selectedOption, setSelectedOption] = useState('');
   const [betAmount, setBetAmount] = useState('');
+  const [showConfirmation, setShowConfirmation] = useState(false);
   const isOptionSelected = selectedOption !== '';
 
   const handleRadioChange = (event) => {
@@ -29,56 +31,70 @@ const BetModal = ({ isOpen, closeModal, handleBetAmount, data }) => {
     }
 
     handleBetAmount(selectedOption, betAmount);
-    setBetAmount(''); // Reset the betAmount state after submitting
-    closeModal();
+    setBetAmount('');
+    setShowConfirmation(true);
+  };
+
+  const handleConfirmationClose = () => {
+    setShowConfirmation(false);
   };
 
   const handleModalClose = () => {
-    setBetAmount(''); // Reset the betAmount state when closing the modal
+    setBetAmount('');
     closeModal();
   };
 
   return (
-    <div className={`modal-overlay ${isOpen ? 'is-open' : ''}`}>
-      <div className={`modal-content ${isOpen ? 'is-open' : ''}`}>
-        <div className="modal-header">
-          <button onClick={handleModalClose} className="bet-modal-close-button">X</button>
-          <h3>{title}</h3>
-        </div>
-        <div className="modal-body">
-          <h4>{Number(price).toLocaleString(undefined, { maximumFractionDigits: 0, minimumFractionDigits: 0 })}</h4>
-          {choices && choices.length > 0 && (
-            <div className="choice-container">
-              {choices.map((choice, index) => (
-                <div key={index} className="choice-selection">
-                  <input
-                    type="radio"
-                    id={`choice-${index}`}
-                    name={title}
-                    value={choice}
-                    onChange={handleRadioChange}
-                  />
-                  <label htmlFor={`choice-${index}`}>{choice}</label>
-                </div>
-              ))}
-            </div>
-          )}
-          <form onSubmit={handleSubmit} className="entry-form">
-            <input
-              type="number"
-              name="betAmount"
-              min="1"
-              required
-              value={betAmount}
-              onChange={handleBetInputChange}
-              className="bet-amount-input"
-              placeholder="Wager Amount"
-            />
-            <button type="submit" disabled={!isOptionSelected}>Submit</button>
-          </form>
+    <>
+      <div className={`modal-overlay ${isOpen ? 'is-open' : ''}`}>
+        <div className={`modal-content ${isOpen ? 'is-open' : ''}`}>
+          <div className="modal-header">
+            <button onClick={handleModalClose} className="bet-modal-close-button">X</button>
+            <h3>{title}</h3>
+          </div>
+          <div className="modal-body">
+            <h4>{Number(price).toLocaleString(undefined, { maximumFractionDigits: 0, minimumFractionDigits: 0 })}</h4>
+            {choices && choices.length > 0 && (
+              <div className="choice-container">
+                {choices.map((choice, index) => (
+                  <div key={index} className="choice-selection">
+                    <input
+                      type="radio"
+                      id={`choice-${index}`}
+                      name={title}
+                      value={choice}
+                      onChange={handleRadioChange}
+                    />
+                    <label htmlFor={`choice-${index}`}>{choice}</label>
+                  </div>
+                ))}
+              </div>
+            )}
+            <form onSubmit={handleSubmit} className="entry-form">
+              <input
+                type="number"
+                name="betAmount"
+                min="1"
+                required
+                value={betAmount}
+                onChange={handleBetInputChange}
+                className="bet-amount-input"
+                placeholder="Wager Amount"
+              />
+              <button type="submit" disabled={!isOptionSelected}>Submit</button>
+            </form>
+          </div>
         </div>
       </div>
-    </div>
+      {showConfirmation && (
+        <BetConfirmationModal
+          title={title}
+          choice={selectedOption}
+          betAmount={betAmount}
+          closeModal={handleConfirmationClose}
+        />
+      )}
+    </>
   );
 };
 
